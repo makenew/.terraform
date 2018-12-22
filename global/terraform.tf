936eca58c9ee620ac645f9589ce1183e7305e7e6
Evan Sosenko <razorx@evansosenko.com>
2018-12-21 19:04:10 -0600

    Add global terraform file

---
+++ global/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "= 0.12"
+  backend "s3" {
+    bucket = "makenew-terraform-state-0"
+    dynamodb_table = "makenew-terraform-state-0"
+    key = "environments/global.tfstate"
+    region = "us-east-1"
+    encrypt = "true"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.40.0"
+  region = "us-east-1"
+}
