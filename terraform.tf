7babc02ff7cf10d930ed7e88d0d280ad717ca952
Evan Sosenko <razorx@evansosenko.com>
2018-12-21 22:55:38 -0600

    Rename state file

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "makenew-terraform-state-0"
     dynamodb_table = "makenew-terraform-state-0"
-    key = "environments/global.tfstate"
+    key = "root.tfstate"
     region = "us-east-1"
     encrypt = "true"
   }
